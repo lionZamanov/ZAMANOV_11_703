95a51d7115b3bc3d89178ad4fa0aeac9f5636898
lionZamanov <lionzamanov@gmail.com>
2017-09-27 15:40:10 +0300

    add LessonsFor

---
+++ LessonFor_1/src/ru/itis/Main.java
@@ -0,0 +1,19 @@
+package ru.itis;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int i = a;
+        while (i <= b) {
+                System.out.print(i + " ");
+                i++;
+        }
+
+    }
+}
