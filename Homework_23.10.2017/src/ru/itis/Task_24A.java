ba4a615d43a2414010edf07a2a7052a2cbc271a7
lionZamanov <32329189+lionZamanov@users.noreply.github.com>
2017-10-24 16:30:51 +0300

    Update Task_24A.java

--- Homework_23.10.2017/src/ru/itis/Task_24A.java
+++ Homework_23.10.2017/src/ru/itis/Task_24A.java
@@ -24,23 +24,17 @@ public class Task_24A {
         b2 = b1*b1 - c1*c1;
         c2 = c1*b1 + c1*b1;
         t = c2/2;
-        s = c1 + t;
+        s = c1;
 
         while(Math.abs(t) >= e){
+            s += t;
             b0 = b2;
             c0 = c2;
             b2 = b1*b0 - c1*c0;
             c2 = c0*b1 + c1*b0;
             t *= c2/(n*c0); //неверная логика
-            s += t;
             n++;
         }
         System.out.println(s);
     }
 }
-
-/*
-
- b0 = cos
-
- */
