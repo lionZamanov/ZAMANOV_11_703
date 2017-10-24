e635f6b9cea9226070b57bfdb7aedc4a2494b3d4
lionZamanov <32329189+lionZamanov@users.noreply.github.com>
2017-10-24 20:49:06 +0300

    Update Task_24B.java

--- Homework_23.10.2017/src/ru/itis/Task_24B.java
+++ Homework_23.10.2017/src/ru/itis/Task_24B.java
@@ -19,7 +19,7 @@ public class Task_24B {
                 c1 = Math.sin(x),
                 c2;
         int
-                n = 3;
+                n = 2;
 
         b2 = b1*b1 - c1*c1; //Находим для cos2x
         c2 = c1*b1 + c1*b1;
@@ -27,10 +27,10 @@ public class Task_24B {
         c0 = c2;
         b2 = b1*b0 - c1*c0; //Находим для cos3x
         c2 = c0*b1 + c1*b0;
-        t = b2;
-        s = b1 + t/2; // Суммируем
+        t = b2/2;
+        s = b1; // Суммируем
         while(Math.abs(t) >= e){
-
+            s += p*t;
             b0 = b2;
             c0 = c2;
             b2 = b1*b0 - c1*c0;//Находим для cos(n+1)x
@@ -39,9 +39,8 @@ public class Task_24B {
             c0 = c2;
             b2 = b1*b0 - c1*c0;//Находим для cos(n+2)x
             c2 = c0*b1 + c1*b0;
-            t = b2/n;  //неверная логика
-            s += p*t;
             n++;
+            t = b2/n;  //неверная логика
         }
         System.out.println(s);
     }
