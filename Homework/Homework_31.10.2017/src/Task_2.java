2d84c83fc7988eb2a9352042cef6d0d71fe379ab
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-31 21:54:51 +0300

    Update Task_2.java

--- Homework/Homework_31.10.2017/src/Task_2.java
+++ Homework/Homework_31.10.2017/src/Task_2.java
@@ -1,5 +1,5 @@
-// Найти кол-во чисел м/д наиб. наим. числами
-import java.util.*;
+// Найти кол-во чисел м/д наиб. наим. числами  //не количество, а сумма.
+import java.util.*; //-
 
 public class Task_2 {
     public static void main(String[] args) {
@@ -7,7 +7,7 @@ public class Task_2 {
         int n = 10,
                 max,
                 maxi = 0,
-                mini = 1,
+                mini = 1,  //почему?
                 min;
         int[] a = new int[n];
 
@@ -18,14 +18,14 @@ public class Task_2 {
         System.out.println();
 
         max = a[0];
-        min = a[1];
+        min = a[1];  //почему?
         if(max < min){ //Смена значений
             max -= min;
             min += max;
-            max = min - max;
+            max = min - max; //что за жонглирование?
         }
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) { 
             if(a[i] > max){
                 max = a[i];
                 maxi = i;
@@ -37,6 +37,6 @@ public class Task_2 {
 
         System.out.println(max);
         System.out.println(min);
-        System.out.println(Math.abs(mini - maxi - 1));
+        System.out.println(Math.abs(mini - maxi - 1)); //какая задача решалась?
     }
 }
