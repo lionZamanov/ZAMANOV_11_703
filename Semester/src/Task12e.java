157b3ce7bc1e1c3219ac92b059a038d0db7d881a
lionZamanov <lionzamanov@gmail.com>
2017-11-06 00:11:21 +0300

    Semestr

--- Semester/src/Task12e.java
+++ Semester/src/Task12e.java
@@ -1,21 +1,40 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Task12e {
 
     public static void main(String[] args) {
 	// 1.2 E
         Scanner in = new Scanner(System.in);
-        double e = 2.74,
-                x,
-                h = in.nextDouble(),
-                b = in.nextDouble(),
-                a;
-        for (int i = 0; i < 3; i++) {
-            a = in.nextDouble();
-            x = b + i*h;
-            System.out.println(F(x,a,e));
+        double
+                z0 = in.nextDouble(),
+                z1 = in.nextDouble(),
+                fi = 0,
+                h = in.nextDouble();
+        int
+            n = in.nextInt(),
+            m = in.nextInt();
+        double[] z = new double[n];
+        z[0] = z0;
+        z[1] = z1;
+
+
+        for (int i = 2; i < n; i++) {
+            z[i] = F(z[i - 1],z[i - 2]);
+
+        }
+        System.out.println(Arrays.toString(z));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if((z[j] >= (i - 1)*h) && (z[j] < i*h)){
+                    fi++;
+                }
+            }
+            System.out.println(fi);
+            fi = 0;
         }
     }
-    public static double F(double a, double x, double e){
+    public static double F(double a, double x){
+        double e = 2.718;
         double res = (Math.pow(e,x) + Math.pow(e,-a))/(1 + a*a + x*x);
         return res;
     }
---
+++ Semester/src/Task13d.java
@@ -0,0 +1,49 @@
+// 1.3
+
+import java.util.*;
+
+public class Task13d {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean f = false;
+        double
+            a0 = in.nextDouble(),
+                b0 = in.nextDouble(),
+                t = 0,
+                e = in.nextDouble();
+        int
+            n = in.nextInt();
+        double[]
+            a = new double[n],
+                b = new double[n];
+        a[0] = a0;
+        b[0] = b0;
+        for (int i = 1; i < n; i++) {
+            t = F(a0, b0);
+            a0 = t;
+            b0 = t;
+            a[i] = a0;
+            b[i] = b0;
+        }
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(f(a[i],b[j]) < e){
+                    f = true;
+                }
+            }
+            if(f){
+                System.out.println(a[i]);
+            }
+            f = false;
+        }
+    }
+    public static double F(double a, double x){
+        return Math.sqrt(2 + Math.sin(a + x)) + Math.sqrt(2 - Math.cos(a + x));
+    }
+    public static double f(double a, double b){
+        return a+b;
+    }
+
+}
