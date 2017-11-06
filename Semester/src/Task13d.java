26ab33dcbeb9ca43b97473ce94680d88fba37710
lionZamanov <lionzamanov@gmail.com>
2017-11-07 01:27:54 +0300

    Semestr

--- Semester/src/Task13d.java
+++ Semester/src/Task13d.java
@@ -19,11 +19,8 @@ public class Task13d {
         a[0] = a0;
         b[0] = b0;
         for (int i = 1; i < n; i++) {
-            t = F(a0, b0);
-            a0 = t;
-            b0 = t;
-            a[i] = a0;
-            b[i] = b0;
+            a[i] = F(a[i-1],b[i-1]);
+            b[i] = G(a[i-1],b[i-1]);
         }
         System.out.println(Arrays.toString(a));
         System.out.println(Arrays.toString(b));
@@ -42,8 +39,11 @@ public class Task13d {
     public static double F(double a, double x){
         return Math.sqrt(2 + Math.sin(a + x)) + Math.sqrt(2 - Math.cos(a + x));
     }
+    public static double G(double a, double x){
+        return (Math.pow(Math.E,x) + Math.pow(Math.E,-a))/(1 + a*a + x*x);
+    }
     public static double f(double a, double b){
-        return a+b;
+        return Math.abs(a-b);
     }
 
 }
