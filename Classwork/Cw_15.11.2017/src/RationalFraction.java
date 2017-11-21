cc77f844083d4f7745c36b389f5a572288cf42ec
lionZamanov <lionzamanov@gmail.com>
2017-11-21 17:47:55 +0300

    cw

--- Classwork/Cw_15.11.2017/src/RationalFraction.java
+++ Classwork/Cw_15.11.2017/src/RationalFraction.java
@@ -1,6 +1,6 @@
 public class RationalFraction {
-    private int n;
-    private int m;
+    public int n;
+    public int m;
 
     public RationalFraction(){
         n = 0;
@@ -11,10 +11,11 @@ public class RationalFraction {
         if(m != 0) {
             this.m = m;
         }else this.m = 1;
+        this.reduce();
     }
-    void reduce(){
-        int a = n;
-        int b = m;
+    public int NOD(int a, int b){
+        a = Math.abs(a);
+        b = Math.abs(b);
         while(a != b){
             if(a > b){
                 a -= b;
@@ -22,8 +23,97 @@ public class RationalFraction {
                 b -= a;
             }
         }
-        n /= a;
-        m /= b;
+        return a;
     }
-    public 
+    /*public int NOK(int a, int b){
+        return a*b/NOD(a,b);
+    }*/
+    void reduce(){
+        int k = NOD(m,n);
+        n /= k;
+        m /= k;
+    }
+    public RationalFraction add(RationalFraction fraction){
+        RationalFraction r = new RationalFraction();
+        if(r.m == fraction.m) {
+            r.n = this.n + fraction.n;
+            r.m = this.m + fraction.m;
+        }else{
+            r.n = this.n * fraction.m + fraction.n * this.m;
+            r.m = this.m * fraction.m;
+        }
+        r.reduce();
+        return r;
+    }
+    public void add2(RationalFraction fraction){
+        if(this.m == fraction.m) {
+            this.n += fraction.n;
+            this.m += fraction.m;
+        }else{
+            this.n = this.n * fraction.m + fraction.n * this.m;
+            this.m = this.m * fraction.m;
+        }
+        this.reduce();
+    }
+    public RationalFraction sub(RationalFraction fraction){
+        RationalFraction r = new RationalFraction();
+        if(r.m == fraction.m) {
+            r.n = this.n - fraction.n;
+            r.m = this.m - fraction.m;
+        }else{
+            r.n = this.n * fraction.m - fraction.n * this.m;
+            r.m = this.m * fraction.m;
+        }
+        r.reduce();
+        return r;
+    }
+    public void sub2(RationalFraction fraction){
+        if(this.m == fraction.m) {
+            this.n -= fraction.n;
+            this.m -= fraction.m;
+        }else{
+            this.n = this.n * fraction.m - fraction.n * this.m;
+            this.m = this.m * fraction.m;
+        }
+        this.reduce();
+    }
+    public RationalFraction mult(RationalFraction fraction){
+        RationalFraction r = new RationalFraction();
+        r.n = this.n * fraction.n;
+        r.m = this.m * fraction.m;
+        r.reduce();
+        return r;
+    }
+    public void mult2(RationalFraction fraction){
+        this.n *= fraction.n;
+        this.m *= fraction.m;
+        this.reduce();
+    }
+    public RationalFraction div(RationalFraction fraction){
+        RationalFraction r = new RationalFraction();
+        r.n = this.n * fraction.m;
+        r.m = this.m * fraction.n;
+        r.reduce();
+        return r;
+    }
+    public void div2(RationalFraction fraction){
+        this.n *= fraction.m;
+        this.m *= fraction.n;
+        this.reduce();
+    }
+
+    @Override
+    public String toString() {
+        return n + "/" + m;
+    }
+    public double value(){
+        return (double)n/m;
+    }
+    public boolean equals(RationalFraction fraction){
+        fraction.reduce();
+        return this.n == fraction.n && this.m == fraction.m;
+    }
+    public int numberPart(){
+        return this.n/this.m;
+    }
 }
--- Homework/Java_21.11.2017/src/Task_38.java
+++ Homework/Java_21.11.2017/src/Task_38.java
@@ -7,28 +7,33 @@ public class Task_38 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Random r = new Random();
-        int n = 7,
+        int n = 3,
+                k1,
+                k2,
                 K;
         int[][] matrix = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j] = r.nextInt(50)+1;
+                matrix[i][j] = r.nextInt(10)+1;
             }
         }
-       /* showMatrix(matrix, n);
+        showMatrix(matrix, n);
+        System.out.println();
         for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = i; k < n - 1; k++) {
-                    K = NOK(matrix[i][k],matrix[i + 1][k]);
+            for (int j = i + 1; j < n; j++) {
+                K = NOK(matrix[i][i],matrix[j][i]);
+                k1 = K/matrix[j][i];
+                k2 = K/matrix[i][i];
+                for (int k = i; k < n; k++) {
+                    matrix[j][k] = matrix[j][k]* - matrix[i][k]*;
                 }
             }
         }
-*/
-        System.out.println(NOK(-8,9));
+        showMatrix(matrix, n);
     }
     public static int NOK(int x, int y){
-        int a = x;
-        int b = y;
+        int a = Math.abs(x);
+        int b = Math.abs(y);
         while (a != b) {
             if(a > b) {
                 a -=b;
