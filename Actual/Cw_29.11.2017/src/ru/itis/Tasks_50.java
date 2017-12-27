f6d290383aa35c821b728094803011e65dcb889b
lion <kotleta.first@gmail.com>
2017-12-27 23:11:04 +0300

    oop and controlwork

--- Actual/Cw_29.11.2017/src/ru/itis/Tasks_50.java
+++ Actual/Cw_29.11.2017/src/ru/itis/Tasks_50.java
@@ -10,7 +10,7 @@ public class Tasks_50 {
         String s2 = in.nextLine();
         boolean flag = true;
         if(s1.length != s2.length){
-        	System.out.println("ИХ нельзя сравнивать")
+        	System.out.println("Их нельзя сравнивать")
         }else{
 	        for (int i = 0; i < s1.length() && flag; i++) {  //ignored  length of rows
 	            if(s1.charAt(i) != s2.charAt(i)){
